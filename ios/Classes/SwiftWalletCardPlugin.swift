05e829667cfc00f9bbc328762f58eb6c753477ff
Matthieu Lagast <matthieu.lagast@gmail.com>
2023-01-18 19:50:34 +0100

    fix debug logs

--- ios/Classes/SwiftWalletCardPlugin.swift
+++ ios/Classes/SwiftWalletCardPlugin.swift
@@ -139,9 +139,9 @@ class PKAddPassButtonNativeView: NSObject, FlutterPlatformView, PKAddPaymentPass
           request.ephemeralPublicKey = ephemeralPublicKey
 
           print(request)
-          print(request.activationData)
-          print(request.encryptedPassData)
-          print(request.ephemeralPublicKey)
+          print(String(decoding: request.activationData!, as: UTF8.self))
+          print(String(decoding: request.encryptedPassData!, as: UTF8.self))
+          print(String(decoding: request.ephemeralPublicKey!, as: UTF8.self))
 
           handler(request)
         })
